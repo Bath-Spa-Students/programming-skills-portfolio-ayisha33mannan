a45926df62029915d86323751a65eef695e072fd
ayisha33mannan <144320259+ayisha33mannan@users.noreply.github.com>
2023-11-18 14:44:09 +0400

    Create Exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -0,0 +1,11 @@
+'''Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.'''
+# find and write the person who wrote the quote
+# assign it to a variable (speach)
+speach = ("Steve Jobs once said,")
+# find and write a quote
+# assign it to a variable (wording)
+wording = ''' "The only way to do great work is to love what you do." '''
+#print the (speach) and (wording)
+print (speach , wording)
