04de4b7f453eb958e3dc05682b6ba8e730983afc
ayisha33mannan <144320259+ayisha33mannan@users.noreply.github.com>
2023-11-18 14:51:33 +0400

    Create Exercise 1.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 1.py
@@ -0,0 +1,14 @@
+'''Store the names of a few of your friends in a list called names.
+Print each person’s name by accessing each element in the list, one at a time.'''
+# creating a variable (name) and creating list of my friends
+Names = ["Zainab", "Sundas" , "Aisha" , "Attia"]
+# printing each persons name at a time
+print("printing each persons name from the list at a time:")
+# printing the 1st name in the list
+print(Names[0])
+# printing the 2snd name in the list
+print(Names[1])
+# printing the 3rd name in the list
+print(Names[2])
+# printing the 4th name in the list
+print(Names[3])
