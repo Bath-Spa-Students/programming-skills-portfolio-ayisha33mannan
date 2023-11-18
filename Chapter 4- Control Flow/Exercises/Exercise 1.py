0792c1ea8c022f625435c2d82b204afde841f137
ayisha33mannan <144320259+ayisha33mannan@users.noreply.github.com>
2023-11-18 15:03:30 +0400

    Create Exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 1.py
@@ -0,0 +1,19 @@
+'''Imagine an alien was just shot down in a game. Create a variable called alien_color and assign
+it a value of 'green', 'yellow', or 'red'.
+•Write an if statement to test whether the alien’s color is green.If it is, print a message that the
+player just earned 5 points.
+•Write one version of this program that passes the if test and another that fails.
+(The version that fails will have no output.)'''
+
+#colour of alien killed will be Green
+alien_color = 'green'
+
+#using if statement
+#if the alien color is green player wins 5 points
+if alien_color == 'green' :
+    print("YAAYY! You killed green alien, 5 points earned")
+    # now the color of the alien killed will be YELLOW
+    #using another color other than green will make u fail
+
+    if alien_color == 'yellow' :
+     print ( "YAAYY! You killed green alien, 5 points earned")
