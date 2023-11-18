f2987e4bdc6797c7af434c3f408cddd07162f39d
ayisha33mannan <144320259+ayisha33mannan@users.noreply.github.com>
2023-11-18 15:15:28 +0400

    Create exercise 3.py

---
+++ Chapter 6- Loops/Exercises/exercise 3.py
@@ -0,0 +1,6 @@
+'''Write a loop that never ends, and run it. (To end the loop, press ctrl-C or close the window displaying the output.)'''
+
+#here is an infinite loop
+while True:
+    print("You are trapped in an infinite loop. Press Ctrl+C or close the window to exit.")
+#to escape press Ctrl+C or close the window
