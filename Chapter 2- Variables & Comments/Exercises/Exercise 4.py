05382786b519385030005aee6f985cba4282b382
ayisha33mannan <144320259+ayisha33mannan@users.noreply.github.com>
2023-11-18 14:47:00 +0400

    Create Exercise 4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 4.py
@@ -0,0 +1,8 @@
+'''Use a variable to represent your favorite number. Then,using that variable,
+ create a message that reveals your favorite number. Print that message.'''
+#variable representing my favorite number
+num_ber = 3
+# creating a message 
+fav = "My favorite number is " + str(num_ber)
+# print altogeter 
+print (fav)
